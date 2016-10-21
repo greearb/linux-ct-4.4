greearb/linux-ct-3.17

ath10k: add irq registers to register dump.
Helps to figure out current hardware state.

Signed-off-by: Ben Greear <greearb@candelatech.com>

drivers/net/wireless/ath/ath10k/debug.c
@@ -331,6 +331,12 @@ void ath10k_debug_read_target_stats(struct ath10k *ar,
 			case PHY_BB_GEN_CONTROLS:
 				stats->phy_bb_gen_controls = __le32_to_cpu(regdump->regpair[i].reg_val);
 				break;
+			case DMA_IMR:
+				stats->dma_imr = __le32_to_cpu(regdump->regpair[i].reg_val);
+				break;
+			case DMA_TXRX_IMR:
+				stats->dma_txrx_imr = __le32_to_cpu(regdump->regpair[i].reg_val);
+				break;
 			case SW_POWERMODE:
 				stats->sw_powermode = __le32_to_cpu(regdump->regpair[i].reg_val);
 				break;
@@ -534,6 +540,10 @@ static ssize_t ath10k_read_fw_regs(struct file *file, char __user *user_buf,
 			 "PHY-MODE-SELECT", fw_regs->phy_bb_mode_select);
 	len += scnprintf(buf + len, buf_len - len, "%30s 0x%08x\n",
 			 "PHY-BB-GEN-CONTROLS", fw_regs->phy_bb_gen_controls);
+	len += scnprintf(buf + len, buf_len - len, "%30s 0x%08x\n",
+			 "DMA-IMR", fw_regs->dma_imr);
+	len += scnprintf(buf + len, buf_len - len, "%30s 0x%08x\n",
+			 "DMA-TXRX-IMR", fw_regs->dma_txrx_imr);
 	len += scnprintf(buf + len, buf_len - len, "%30s 0x%08x\n",
 			 "PCU-BSSID-L32", fw_regs->pcu_bssid_l32);
 	len += scnprintf(buf + len, buf_len - len, "%30s 0x%08x\n",

greearb/linux-ct-4.4

drivers/net/wireless/ath/ath10k/debug.c
@@ -381,6 +381,12 @@ void ath10k_debug_fw_stats_process(struct ath10k *ar, struct sk_buff *skb)
 			case PHY_BB_GEN_CONTROLS:
 				sptr->phy_bb_gen_controls = __le32_to_cpu(regdump->regpair[i].reg_val);
 				break;
+			case DMA_IMR:
+				sptr->dma_imr = __le32_to_cpu(regdump->regpair[i].reg_val);
+				break;
+			case DMA_TXRX_IMR:
+				sptr->dma_txrx_imr = __le32_to_cpu(regdump->regpair[i].reg_val);
+				break;
 			case SW_POWERMODE:
 				sptr->sw_powermode = __le32_to_cpu(regdump->regpair[i].reg_val);
 				break;
@@ -629,6 +635,10 @@ static ssize_t ath10k_read_fw_regs(struct file *file, char __user *user_buf,
 			 "PHY-MODE-SELECT", fw_regs->phy_bb_mode_select);
 	len += scnprintf(buf + len, buf_len - len, "%30s 0x%08x\n",
 			 "PHY-BB-GEN-CONTROLS", fw_regs->phy_bb_gen_controls);
+	len += scnprintf(buf + len, buf_len - len, "%30s 0x%08x\n",
+			 "DMA-IMR", fw_regs->dma_imr);
+	len += scnprintf(buf + len, buf_len - len, "%30s 0x%08x\n",
+			 "DMA-TXRX-IMR", fw_regs->dma_txrx_imr);
 	len += scnprintf(buf + len, buf_len - len, "%30s 0x%08x\n",
 			 "PCU-BSSID-L32", fw_regs->pcu_bssid_l32);
 	len += scnprintf(buf + len, buf_len - len, "%30s 0x%08x\n",